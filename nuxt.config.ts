6d8ed72f0d16476c1f28455dac74cfa4df68502b
biLL Rithy <bill.skun@pathmazing.com>
2023-09-06 13:25:30 +0700

    chore: remove the next@mongoose package

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,7 +5,6 @@ import km from './locale/km.json'
 export default defineNuxtConfig({
   modules: [
     '@pinia/nuxt',
-    'nuxt-mongoose',
     '@nuxtjs/tailwindcss',
     '@vueuse/nuxt',
     '@nuxtjs/i18n',
@@ -13,11 +12,6 @@ export default defineNuxtConfig({
     '@nuxtjs/color-mode',
     '@nuxt/image-edge',
   ],
-  mongoose: {
-    uri: process.env.MONGODB_URI,
-    options: {},
-    modelsDir: 'models',
-  },
   i18n: {
     vueI18n: {
       legacy: false,
